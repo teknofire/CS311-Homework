df2a8bdfa13a3930449d68dc54c9c31312d7b910
Will Fisher <will@teknofire.net>
2014-02-10 19:25:37 -0900

    Initial Commit

--- ksarray-hw2/ksarray.h
+++ ksarray-hw2/ksarray.h
@@ -12,31 +12,145 @@
 #include <algorithm>
 
 template <typename DataType>
-class KSArray {
+class KSArray
+{
+	friend bool operator==(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs);
+	friend bool operator!=(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs);
+
 public:
 	typedef std::size_t size_type;
 	typedef DataType value_type;
 
-	explicit KSArray(size_type size);
+	explicit KSArray(size_type size=10);
 	KSArray(size_type size, value_type value);
+	KSArray(const KSArray<DataType> &rhs);
 	~KSArray();
 
 	/* TODO: define copy ctor, use std::copy */
 
-	size_type size();
+	size_type size() const;
 	value_type * begin();
 	value_type * end();
+	const value_type * begin() const;
+	const value_type * end() const;
 
 	value_type & operator[](size_type index);
-	const value_type & operator[](size_type index) const;
+	const value_type operator[](size_type index) const;
+	KSArray<value_type> & operator=(const KSArray<value_type> &rhs);
+
+	bool operator<(const KSArray<value_type> &rhs);
+	bool operator<=(const KSArray<value_type> &rhs);
+	bool operator>(const KSArray<value_type> &rhs);
+	bool operator>=(const KSArray<value_type> &rhs);
+
 private:
 	size_type _size;
 	value_type * _data;
 };
 
-KSArray<DataType>::KSArray(size_type size): _data(new value_type[size]), _size(size)
+/* global operator defs */
+
+template <typename DataType>
+bool operator==(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs);
+
+template <typename DataType>
+bool operator!=(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs);
+
+/* Class function implementations */
+
+template <typename DataType>
+KSArray<DataType>::KSArray(size_type size): _size(size), _data(new value_type[size])
 {
 
+}
+
+template <typename DataType>
+KSArray<DataType>::KSArray(size_type size, value_type value): _size(size), _data(new value_type[size])
+{
+	for(auto it = begin(); it < end(); ++it)
+	{
+		*it = value;
+	}
+}
+
+template <typename DataType>
+KSArray<DataType>::~KSArray()
+{
+	delete [] _data;
+}
+
+// copy ctor
+template <typename DataType>
+KSArray<DataType>::KSArray(const KSArray<DataType> &rhs) : _size(rhs.size()), _data(new value_type[rhs.size()])
+{
+	operator=(rhs);
+}
+
+template <typename DataType>
+bool operator==(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs) {
+	return (lhs._size == rhs._size && lhs._data == rhs._data);
+}
+
+template <typename DataType>
+bool KSArray<DataType>::operator<(const KSArray<DataType> &rhs)
+{
+	auto lh_it = begin();
+	auto rh_it = rhs.begin();
+
+
+	return *lh_it < *rh_it;
+}
+
+template <typename DataType>
+bool operator!=(const KSArray<DataType> &lhs, const KSArray<DataType> &rhs)
+{
+  return !(lhs == rhs);
+}
+
+//operator=
+template <typename DataType>
+KSArray<DataType> & KSArray<DataType>::operator=(const KSArray<DataType> &rhs)
+{
+	if (this != &rhs)
+	{
+		delete [] _data;
+		_data = new value_type[_size];
+
+		std::copy(rhs.begin(), rhs.end(), _data);
+	}
+
+	return *this;
+}
+
+template <typename DataType>
+typename KSArray<DataType>::value_type * KSArray<DataType>::begin()
+{
+	return &_data[0];
+}
+
+template <typename DataType>
+const typename KSArray<DataType>::value_type * KSArray<DataType>::begin() const
+{
+	return &_data[0];
+}
+
+template <typename DataType>
+const typename KSArray<DataType>::value_type * KSArray<DataType>::end() const
+{
+	return &_data[_size];
+}
+
+template <typename DataType>
+typename KSArray<DataType>::value_type * KSArray<DataType>::end()
+{
+	return &_data[_size];
+}
+
+
+template <typename DataType>
+typename KSArray<DataType>::size_type KSArray<DataType>::size() const
+{
+	return _size;
 }
 
 #endif /* KSARRAY_H_ */
--- wrapper/class_test/class_test/main.cpp
+++ wrapper/class_test/class_test/main.cpp
@@ -8,6 +8,28 @@
 
 #include <iostream>
 
+# include <iostream>
+template <typename T>
+class Wrapper
+{
+    typedef T value_type;
+public:
+    Wrapper(const T &it);
+    T val();
+    
+private:
+    T _data;
+};
+
+template <typename T>
+Wrapper<T>::Wrapper(const T &it):_data(it)
+{}
+
+template <typename T>
+typename Wrapper<T>::value_type Wrapper<T>::val() {
+    return _data;
+}
+
 int main(int argc, const char * argv[])
 {
 
